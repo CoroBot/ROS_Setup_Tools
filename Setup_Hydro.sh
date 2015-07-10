caaedab2e8401afe3f8979663b4dc934ac4fe2a3
bleucanary <cowens@coroware.com>
2015-07-09 22:44:53 -0500

    Starting to add the user tools and submitting for testing.

--- Setup_Hydro.sh
+++ Setup_Hydro.sh
@@ -1,13 +1,59 @@
 #! /bin/sh
 #
 # Setup.sh
-# Copyright (C) 2015 cameron <cameron@Megatron-Virtual>
+# Copyright (C) 2015 CoroWare Robotics Solutions  <www.corobot.net>
+# Author: Cameron Owens <cowens@coroware.com>
 #
 # Distributed under terms of the MIT license.
 #
+# Name: Setup_Hydro.sh
+# Description: ROS Hydro Setup for Ubuntu 14.04
+# Ver: 0.1
+# Plat: Ubuntu 14.04
+
+Ver="0.1"
+
+echo -n "
+
+Welcome to the CoroBot ROS Hydro Setup Utility Ver (Insert Version Var) by CoroWare Robotics Solutions. 
+
+In order to continue the installation process, please review the permissions, warnings, tea leaves, magic eight balls, and license agreement. 
+
+Please, press ENTER to continue
+
+>>>"
+    read dummy
+    more <<EOF
+
+==============================
+CoroBot ROS Hydro Setup Information
+==============================
+Becasue ROS Hydro is not fully supported by the OSRF to run on Ubuntu 14.04, to install it one must install it from source. This script aims to do just that along with installing the other tools and packages that are important/valuable for ROS as well as the CoroBot ROS Stack. 
+
+This shell script follows the general directions for installing ROS Hydro on Ubuntu 14.04 as outlined @ http://wiki.ros.org/hydro/Installation/Trusty
+
+Before installing ROS Hydro, this script also installs some of the following dependencies and tools that are required for installing ROS Hydro
+
+-System updates (sudo apt-get update && sudo apt-get upgrade)
+-Pip: Python package manager.
 
 
-#This shell script is for installing the full ROS Desktop
+
+EOF
+    echo -n"
+Are you ready to install? [yes|no]
+[Proceeed at your own risk...] >>>"
+    read ans
+    if [[ ($ans != "yes") && ($ans != "Yes") && ($ans != "YES") && ($ans !="y") && ($ans != "Y") ]]
+    then
+        echo "Aborting the installation process..."
+	exit 2
+    fi
+
+    echo -n "
+The CoroBot Hydro Installation Setup tool will begin the installation process
+"
+
 
 #Run System Updates and Upgrades
 
@@ -19,7 +65,6 @@ sudo apt-get update && sudo apt-get upgrade -y
 clear
 
 #Setup your Sources
-echo "ROS Hydro is not officially supported on Ubuntu Trusty. That said, it runs very well and CoroWare Robotics is supporting running Hydro on Trusty"
 
 echo "Installing the dependencies"
 sudo apt-get install python-pip
