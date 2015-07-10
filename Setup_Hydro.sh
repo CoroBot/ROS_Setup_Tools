33d502582e9a35c3c97f126fbfe0bcb663af168b
bleucanary <cowens@coroware.com>
2015-07-10 16:54:06 -0500

    Added an OpenCV installer to the script

--- Setup_Hydro.sh
+++ Setup_Hydro.sh
@@ -48,7 +48,7 @@ EOF
 echo -e $COL_RED"
 Are you ready to install? [yes|no]
 [Proceeed at your own risk...] >>>"
-    read ans
+read ans
     if [[ ($ans != "yes") && ($ans != "Yes") && ($ans != "YES") &&
 	    ($ans != "y") && ($ans != "Y") ]]
     then
@@ -73,11 +73,35 @@ sudo apt-get update && sudo apt-get upgrade -y
 
 clear
 
-#Setup your Sources
 
 echo -e $COL_GREEN "Installing the dependencies" $COL_RESET
 sudo apt-get install -y python-pip
-sudo apt-get install -y svn build-essentials mercurial 
+sudo apt-get install -y svn build-essentials mercurial vim emacs screen
+
+echo -e $COL_RED "Getting OpenCV and Installing... Because it's AWESOME"
+
+$COL_RESET
+version="$(wget -q -O - http://sourceforge.net/projects/opencvlibrary/files/opencv-unix | egrep -m1 -o '\"[0-9](\.[0-9]+)+' | cut -c2-)"
+echo "Installing OpenCV" $version
+mkdir OpenCV
+cd OpenCV
+echo "Removing any pre-installed ffmpeg and x264"
+sudo apt-get -qq remove ffmpeg x264 libx264-dev
+echo "Installing Dependenices"
+sudo apt-get -qq install libopencv-dev build-essential checkinstall cmake pkg-config yasm libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils ffmpeg cmake qt5-default checkinstall
+echo "Downloading OpenCV" $version
+wget -O OpenCV-$version.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/$version/opencv-"$version".zip/download
+echo "Installing OpenCV" $version
+unzip OpenCV-$version.zip
+cd opencv-$version
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
+make -j2
+sudo checkinstall
+sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+sudo ldconfig
+echo "OpenCV" $version "ready to be used"
 
 ##Need to update to have user feedback and confirmation dialogs
 
