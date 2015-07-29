1d4f734f5824cc67156e931755e990d7ccb72cef
Cameron <cameron@corobot.(none)>
2015-07-29 09:43:59 -0500

    Fixed a spelling issue.

--- Setup_Groovy.sh
+++ Setup_Groovy.sh
@@ -12,7 +12,7 @@ sudo apt-get update && sudo apt-get upgrade -y
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
 
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
-sudo apt-get udpate
+sudo apt-get update
 sudo apt-get install ros-groovy-desktop-full -y
 sudo rosdep init
 rosdep update
